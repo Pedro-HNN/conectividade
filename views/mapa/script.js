80fbfcc6b72037c2809820c4ee2cdc734791aa91
Pedro Henrique Neiva Nunes <88400546+Pedro-HNN@users.noreply.github.com>
2021-09-12 03:47:31 -0300

    Frontend Mapa completo
    Agora só falta integrar com a API

--- views/mapa/script.js
+++ views/mapa/script.js
@@ -1,53 +1,75 @@
 (function($) {
 	
 	$(document).ready(function(){
-		//toda vez que o mapa È clicado
+		//toda vez que o mapAa ÔøΩ clicado
 		$('#map .state').click(function(){
 			var estado = $(this).attr('data-state');//pega nome do estado
-			var codigoIBGE = $(this).attr('cod-state');//pega cÛdigo IBGE para as consultas API
-			var anoMes = $('#datepicker').val();//pega mÍs e ano
+			var codigoIBGE = $(this).attr('cod-state');//pega c√≥digo IBGE para as consultas API
+			var anoMes = $('#datepicker').val();//pega m√™s e ano
 			
+			$('#info-state').css({ 'display' : 'block'});
 			
-
 			if(anoMes == ''){
-				$('.info-state').html('<h3>Informe a data que quer buscar!</h3>')
+				$('#info-state').html('<h3>Informe a data!</h3>')
 			}else{
 				
-				//coloca a vari·vel no padr„o do IBGE para pesquisa da API
 				var anoMes = anoMes.split("-");
 			
-				//pega sÛ ano e mÍs
+				//pega ano e m√™s
 				var ano = anoMes[1];
 				var mes = anoMes[0];
-				
+
 				var anoMes = anoMes[1]+anoMes[0];
 				
-				//TODO: PEGAR API DO AUXILIO EMERGENCIAL E DO BOLSA FAMÕLIA E COLOCAR NAS VARI¡VEIS
-				//VARI¡VEIS PARA PEGA A API: codigoIBGE e anoMes (p·gina È sempre 1)
-				
-				var valorBolsa;//valor total do Bolsa FamÌlia
-				var beneficiariosBolsa;//quantidade de beneficiarios Bolsa FamÌlia
+				var valorBolsa;//valor total do Bolsa Fam√≠lia
+				var beneficiariosBolsa;//quantidade de beneficiarios Bolsa Fam√≠lia
 				
 				var valorAuxilio;//valor total do auxilio
 				var beneficiariosAuxilio;//quantidade de beneficiarios Auxilio
 				
-				//envio frontend para teste
-				$('.info-state').html('<h3>'+estado+'</h3><ul><li>'+codigoIBGE+'</li><li>'+anoMes+'</li></ul>')
-				
-				//envio frontend oficial (precisa coletar os dados primeiro)
-				//$('.info-state').html('<h3>'+estado+'</h3><h2>Bolsa FamÌlia em '+ mes+"/"+ano +'</h2><hr/><ul><li>Quantidade de Benifici·rios pelo Bolsa FamÌlia: '+beneficiariosBolsa+'</li><li>Total de dinheiro usado no Bolsa FamÌlia: '+valorBolsa+'</li></ul><h2>Auxilio Emergencial em '+ mes+"/"+ano +'</h2><ul><li>Quantidade de Benifici·rios pelo Auxilio Emergencial: '+beneficiariosAuxilio+'</li><li>Total de dinheiro usado no Auxilio Emergencial: '+valorAuxilio+'</li></ul>')
-
-			
+				if(ano < 2020 | ano == 2020 & mes < 4){
+					
+					$('#info-state').html('<h2>'+estado+' '+mes+'/'+ano+'</h2><hr/><h3>BOLSA FAM√çLIA</h3><ul style="text-align:left;"><li>Quantidade de Benifici√°rios: '+beneficiariosBolsa+'</li><li>Total de dinheiro usado: '+valorBolsa+'</li></ul>');
+				}else{
+					$('#info-state').html('<h2>'+estado+' '+mes+'/'+ano+'</h2><hr/><h3>BOLSA FAM√çLIA</h3><ul style="text-align:left;"><li>Quantidade de Benifici√°rios: '+beneficiariosBolsa+'</li><li>Total de dinheiro usado: '+valorBolsa+'</li></ul ><hr/><h3>AUXILIO EMERGENCIAL</h3><ul style="text-align:left;"><li>Quantidade de Benifici√°rios: '+beneficiariosAuxilio+'</li><li>Total de dinheiro usado: '+valorAuxilio+'</li></ul>');
+				}
 			}
-			
-			''
-			//teste
-			console.log(estado + " " + codigoIBGE + " " + mesAno);   
 	    });
 
+		$('#btn-consultar').click(function(){
+			var estado = $('#select-estados option:selected').text();//pega nome do estado
+			var codigoIBGE = $('#select-estados').val();//pega c√≥digo IBGE para as consultas API
+			var anoMes = $('#datepicker').val();//pega m√™s e ano
+			
+			console.log(estado + "/" + codigoIBGE + "/"+ anoMes)
+			
+			$('#info-state').css({ 'display' : 'block'});
+			if(anoMes == ''){
+				$('#info-state').html('<h3>Informe a data!</h3>');
+			}else if(codigoIBGE == 'null'){
+				$('#info-state').html('<h3>Informe o estado!</h3>');
+			}else{
+				var anoMes = anoMes.split("-");
+			
+				//pega ano e m√™s
+				var ano = anoMes[1];
+				var mes = anoMes[0];
+				
+				var anoMes = anoMes[1]+anoMes[0];
 		
-
-	 
-
+				var valorBolsa;//valor total do Bolsa Fam√≠lia
+				var beneficiariosBolsa;//quantidade de beneficiarios Bolsa Fam√≠lia
+				
+				var valorAuxilio;//valor total do auxilio
+				var beneficiariosAuxilio;//quantidade de beneficiarios Auxilio
+				
+				if(ano < 2020 | ano == 2020 & mes < 4){
+					
+					$('#info-state').html('<h2>'+estado+' '+mes+'/'+ano+'</h2><hr/><h3>BOLSA FAM√çLIA</h3><ul style="text-align:left;"><li>Quantidade de Benifici√°rios: '+beneficiariosBolsa+'</li><li>Total de dinheiro usado: '+valorBolsa+'</li></ul>');
+				}else{
+					$('#info-state').html('<h2>'+estado+' '+mes+'/'+ano+'</h2><hr/><h3>BOLSA FAM√çLIA</h3><ul style="text-align:left;"><li>Quantidade de Benifici√°rios: '+beneficiariosBolsa+'</li><li>Total de dinheiro usado: '+valorBolsa+'</li></ul ><hr/><h3>AUXILIO EMERGENCIAL</h3><ul style="text-align:left;"><li>Quantidade de Benifici√°rios: '+beneficiariosAuxilio+'</li><li>Total de dinheiro usado: '+valorAuxilio+'</li></ul>');
+				}
+			}
+		});
 	});
 })(jQuery);
