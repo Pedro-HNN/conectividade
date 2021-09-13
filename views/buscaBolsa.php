8e72d85ad63ae176cd97541cf7f7745559914c9d
Pedro Henrique Neiva Nunes <88400546+Pedro-HNN@users.noreply.github.com>
2021-09-13 01:42:56 -0300

    Add files via upload

--- views/buscaBolsa.php
+++ views/buscaBolsa.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/js/bootstrap-datepicker.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/css/datepicker.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../views/style/style-busca.css">
+    <link rel="stylesheet" href="../../views/style/style-busca.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -26,46 +26,40 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                <div id="pesquisa-bolsa" align="center">
-                    <div class="card custom-card shadow p-2 mb-5 bg-white rounded h-100" style="margin-top:20px;max-width: 25rem; border-color: #0094d9;">
-                        <div class="card-body text-dark ">
-                            <h5 class="card-title">Pesquise aqui sua situação do Bolsa Família!</h5>
-                            <hr />
-                            <div class="form-group">
-                                <input type="text" class="btn btn-lg" id="cpf-bolsa" placeholder="Digite seu CPF!" style="background-color: #0094d9;border-color: #0094d9; color:#ffffff;" />
-                                <input type="text" class="btn btn-lg " name="datepicker" id="datepicker" placeholder="Mês Competência" style="background-color: #0094d9;border-color: #0094d9; color:#ffffff;margin-top:5px;" />
-                                <button id="btn-consultar" class="btn btn-lg shadow p-4 lg-5 rounded" value="submit" style="background-color: #0094d9;border-color:#0094d9;margin-top:5px;color:#ffffff;">Consultar</button>
-                            </div>
-                        </div> 
-                    </div>
+        	<div id="pesquisa-bolsa" align="center">
+                <div class="card custom-card shadow p-3  bg-white rounded h-100" style="margin-top:20px;max-width: 25rem; border-color: #0094d9;">
+                    <div class="card-body text-dark ">
+                        <h2 class="card-title"><strong>Pesquise aqui sua situação do Bolsa Família!</strong></h2>
+                        <hr />
+                        <div class="form-group">
+                            <input type="text" class="btn btn-lg" id="cpf-bolsa" placeholder="Digite seu CPF!" style="background-color: #0094d9;border-color: #0094d9; color:#ffffff;" />
+                            <input type="text" class="btn btn-lg " name="datepicker" id="datepicker" placeholder="Mês Competência" style="background-color: #0094d9;border-color: #0094d9; color:#ffffff;margin-top:5px;" />
+                            <button id="btn-consultar" class="btn btn-lg shadow p-4 lg-5 rounded" value="submit" style="background-color: #0094d9;border-color:#0094d9;margin-top:15px;color:#ffffff;">Consultar</button>
+                        </div>
+                    </div> 
                 </div>
             </div>
-            <div class="col-md-6">
-            	<div id="resultado-bolsa" align="center">
-                	<div class="card text-dark shadow p-3 mb-5 bg-white rounded" style="max-width: 25rem;margin-top:20px;border-color:#0094d9;">
-                        <div id="bolsa-info">
-                            <div id="nome"></div>
-                            <div id="estado"></div>
-                            <div id="valor"></div>
-                            <div id="dependentes"></div>
-                        </div>
-                    </div>
-            	</div>
         </div>
-	</div>
+        <div class="row" style="margin-top:20px;" align="center">
+            <div id="resultado-bolsa" align="center">
+            	<div class="card text-dark shadow p-3 bg-white rounded" style="max-width: 25rem;margin-top:20px;border-color:#0094d9;">
+                    <div id="bolsa-info">
+                        <div id="nome"></div>
+                        <div id="estado"></div>
+                        <div id="valor"></div>
+                        <div id="dependentes"></div>
+                    </div>
+                </div>
+    		</div>
+        </div>
+    </div>
         
-            
-	</div>
 
     <?php include_once 'footer.html' ?>
 
 </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/2.3.2/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-<script src="../scripts/script.js"></script>
+
+<script src="../../scripts/script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/js/bootstrap-datepicker.min.js"></script>
 <script>
     $("#datepicker").datepicker({
