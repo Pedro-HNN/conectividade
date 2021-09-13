6571ff976e2caa4fbf0c71d266489d012dfa2864
Pedro Henrique Neiva Nunes <88400546+Pedro-HNN@users.noreply.github.com>
2021-09-12 21:41:14 -0300

    Delete scripts directory

--- scripts/bootstrap.php
+++
@@ -1,2 +0,0 @@
-<script scr="./vendor/twbs/bootstrap/dist/css/bootstrap.css"></script>
-<script scr="./vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
--- scripts/jquery.php
+++
@@ -1,3 +0,0 @@
-
-<script src="../vendor/components/jquery/jquery.min.js"></script>
-<script src="../scripts/jqueryUI/jquery-ui.js"></script>
